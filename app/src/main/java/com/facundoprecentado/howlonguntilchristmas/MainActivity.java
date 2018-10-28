2bec737da8924082b0d9ad7059f12deff083bdcc
facundop <facundo.precentado@mercadolibre.com>
2018-10-28 19:48:58 -0300

    Add Ad onClick

--- app/src/main/java/com/facundoprecentado/howlonguntilchristmas/MainActivity.java
+++ app/src/main/java/com/facundoprecentado/howlonguntilchristmas/MainActivity.java
@@ -13,6 +13,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -20,12 +27,16 @@ import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RewardedVideoAdListener {
 
-    TextView timeUntilText;
-    Button calculateTimeButton;
+    private RewardedVideoAd mRewardedVideoAd;
+    private TextView timeUntilText;
+    private Button calculateTimeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,17 +45,9 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent sendIntent = new Intent();
-                sendIntent.setAction(Intent.ACTION_SEND);
-                sendIntent.putExtra(Intent.EXTRA_TEXT, "Find out how long until Christmas!");
-                sendIntent.setType("text/plain");
-                startActivity(sendIntent);
-            }
-        });
+        // Use an activity context to get the rewarded video instance.
+        mRewardedVideoAd = MobileAds.getRewardedVideoAdInstance(this);
+        mRewardedVideoAd.setRewardedVideoAdListener(this);
 
         timeUntilText = (TextView) findViewById(R.id.timeUntilText);
         calculateTimeButton = (Button) findViewById(R.id.calculateTimeButton);
@@ -55,10 +58,47 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        loadRewardedVideoAd();
+    }
+
+    private void loadRewardedVideoAd() {
+        mRewardedVideoAd.loadAd("ca-app-pub-3940256099942544/5224354917",
+                new AdRequest.Builder().build());
     }
 
     private void calculateTime() {
+        if (mRewardedVideoAd.isLoaded()) {
+            mRewardedVideoAd.show();
+        }
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_share) {
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "Find out how long until Christmas with this App!");
+            sendIntent.setType("text/plain");
+            startActivity(sendIntent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRewarded(RewardItem reward) {
+        // Load a new Ad
+        loadRewardedVideoAd();
+
+        // Reward the user.
         LocalDateTime ldt = LocalDateTime.now();
         ZonedDateTime now = ldt.atZone(ZoneId.of(TimeZone.getDefault().getID()));
         ZonedDateTime christmas = ZonedDateTime.of ( LocalDate.of ( now.getYear() , 12 , 24 ) , LocalTime.of ( 0 , 0 ) , ZoneId.of (TimeZone.getDefault().getID()));
@@ -92,25 +132,32 @@ public class MainActivity extends AppCompatActivity {
         }.start();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void showError() {
+        Toast.makeText(this, "An error occurred. Please try again.", Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onRewardedVideoAdLeftApplication() {
+        Toast.makeText(this, "An error occurred. Please try again.", Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    public void onRewardedVideoAdClosed() { loadRewardedVideoAd(); }
+
+    @Override
+    public void onRewardedVideoAdFailedToLoad(int errorCode) {
+        loadRewardedVideoAd();
+    }
+
+    @Override
+    public void onRewardedVideoAdLoaded() {}
+
+    @Override
+    public void onRewardedVideoAdOpened() {}
+
+    @Override
+    public void onRewardedVideoStarted() {}
+
+    @Override
+    public void onRewardedVideoCompleted() {}
 }
