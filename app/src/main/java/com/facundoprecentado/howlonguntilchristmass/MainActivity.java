3365cbf08e32e506fdd48b31298ea325e1231e34
facundop <facundo.precentado@mercadolibre.com>
2018-10-28 15:49:51 -0300

    Calculate time until Christmass

--- app/src/main/java/com/facundoprecentado/howlonguntilchristmass/MainActivity.java
+++ app/src/main/java/com/facundoprecentado/howlonguntilchristmass/MainActivity.java
@@ -1,5 +1,6 @@
 package com.facundoprecentado.howlonguntilchristmass;
 
+import android.icu.util.TimeZone;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.Days;
+import org.joda.time.Hours;
+import org.joda.time.Minutes;
+
+import java.sql.Time;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView timeUntilText;
+    Button calculateTimeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +41,31 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        timeUntilText = (TextView) findViewById(R.id.timeUntilText);
+        calculateTimeButton = (Button) findViewById(R.id.calculateTimeButton);
+        calculateTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateTime();
+            }
+        });
+
+    }
+
+    private void calculateTime() {
+        // Get local TimeZone
+        TimeZone tz = TimeZone.getDefault();
+        DateTimeZone localTimeZone = DateTimeZone.forID(tz.getID());
+
+        DateTime start = new DateTime(localTimeZone);
+        DateTime end = new DateTime(start.getYear(), 12, 24, 0, 0, 0, localTimeZone);
+
+        int daysBetweenDates = Days.daysBetween(start, end).getDays();
+        int hoursBetweenDates = Hours.hoursBetween(start, end).getHours();
+        int minutesBetweenDates = Minutes.minutesBetween(start, end).getMinutes();
+
+        timeUntilText.setText("Days Until Christmass: " + daysBetweenDates + " Hours: " + hoursBetweenDates % 24 + " Minutes: " + minutesBetweenDates % 60);
     }
 
     @Override
